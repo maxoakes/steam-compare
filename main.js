14bc3a72ee8a0efe98b0d6e7952287987bca4066
maxoakes <m.oakes017@gmail.com>
2021-05-01 18:44:50 -0700

    Confirmed proxy website for CORS to allow APIs to get fetched

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 document.getElementById("entry-form").addEventListener("submit", function(event)
 {
+    //TODO: this works a proxy website for CORS to allow the api to get fetched. Perhaps there is a more elegent way to do this
     var proxy = "https://still-tor-77449.herokuapp.com/"
+
     var key="386540A52F687754D4E1767230822EDE";
     var appid = document.getElementById("game").value;
     var userid = document.getElementById("username").value;
+
     var url = proxy+'https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key='+key+'&steamid=' + userid + '&format=json';
     event.preventDefault();
     console.log();
+    
     fetch(url, {headers: {'Access-Control-Allow-Origin':'*'}})
     .then(response => {
         console.log(response.json())
