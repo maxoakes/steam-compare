4593f8731b89704d0d14b5f2dc6326874db329ec
maxoakes <m.oakes017@gmail.com>
2021-05-01 17:38:23 -0700

    added working api pull... maybe?

--- main.js
+++ main.js
@@ -1,34 +1,16 @@
 document.getElementById("entry-form").addEventListener("submit", function(event)
 {
+    var proxy = "https://still-tor-77449.herokuapp.com/"
     var key="386540A52F687754D4E1767230822EDE";
     var appid = document.getElementById("game").value;
     var userid = document.getElementById("username").value;
     var url = 'https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key='+key+'&steamid=' + userid + '&format=json';
     event.preventDefault();
-    console.log(url)
+    console.log()
     fetch(url, {mode:'cors'}).then(response => {
-        console.log(response)
+        console.log(response.json())
     })
     .catch(error => {
         console.log(error)
     });
-    // var homeURL = window.location.href+"getuserstats/?"
-    // var userAppID = "413150";
-    // var userPlayerID = "76561198009589509";
-    // var newURL = homeURL+userAppID+'='+userPlayerID;
-    // var req = new XMLHttpRequest();
-    // req.open("GET", newURL, true);
-    // req.addEventListener('load', function(){
-    //     if(req.status>= 200 && req.status<400)
-    //     {
-    //         var response = JSON.parse(req.responseText);
-    //         console.log(response);
-    //     }
-    //     else
-    //     {
-    //         console.log("Error in network request: " + req.statusText);
-    //     }
-    // });
-    // req.send(null);
-    // console.log("end of script");
 });
