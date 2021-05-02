acb4dc33870402f7436a838245305d2b674f4f65
maxoakes <m.oakes017@gmail.com>
2021-05-01 18:41:33 -0700

    added access control allow origin header

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ document.getElementById("entry-form").addEventListener("submit", function(event)
     var key="386540A52F687754D4E1767230822EDE";
     var appid = document.getElementById("game").value;
     var userid = document.getElementById("username").value;
-    var url = 'https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key='+key+'&steamid=' + userid + '&format=json';
+    var url = proxy+'https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key='+key+'&steamid=' + userid + '&format=json';
     event.preventDefault();
     console.log();
     fetch(url, {headers: {'Access-Control-Allow-Origin':'*'}})
