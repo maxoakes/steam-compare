5e08f19f3a0e760c7a8706c7cce0fdc00219d06c
Arturo Bravo <arturo@Arturos-MacBook-Air.local>
2021-05-10 21:10:46 -0700

    moved indexhtml to app.js. Do not believe i did this right

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import './App.css';
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css'
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+//https://reactrouter.com/web/guides/quick-start
+//got started with this template
+function App() {
+  return (
+    <div className='App'>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="header col-xs-12 col-md-12">
+            <h1>Title</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="body col-xs-12 col-md-12">
+            <div className="form-body">
+              <form id="entry-form">
+                <div className="form-group">
+                  <label for="username">Steam ID</label>
+                  <input type="text" className="form-control" id="username" aria-describedby="username-help" placeholder="Enter a Steam ID" name="username" value="scouteriv" />
+                  <small id="username-help" className="form-text">Enter a Steam 'vanity' URL of a user.</small>
+                </div>
+                <div className="form-group">
+                  <label for="game">Steam Game</label>
+                  <input type="text" className="form-control" id="game" aria-describedby="game-help" placeholder="Enter a Steam Game" name="game" value="Stardew Valley" />
+                  <small id="game-help" className="form-text">Enter a game or app that is available on Steam.</small>
+                </div>
+                <button type="submit" id="entry-submit" className="btn btn-primary">Search!</button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="footer col-xs-12 col-md-12">
+            <hr>
+            <p className="footer-links">Link | Link | Link | Link | Link</p>
+            <p className="footer-authors font-weight-light">Created by</p>
+            <p className="footer-legal text-muted ">Steam and the Steam logo are trademarks of Valve Corporation. All other trademarks are property of their respective owners.
+              <a href="\">Fair use disclaimer</a> 
+            </p>
+            </hr>
+          </div>
+        </div>
+     </div>
+    </div>
+    );
+  }
+
+
+export default App;
