f428252bd86b5d668962d37221764d3dc5ccb025
Arturo Bravo <arturo@Arturos-MacBook-Air.local>
2021-05-10 21:18:38 -0700

    Added bootstrap. still looking for why it is not rendering

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './main.css';
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.css'
 import{
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './main.css'
 //this doesnt work yet
-//import App from './App';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
