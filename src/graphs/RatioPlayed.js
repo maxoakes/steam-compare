ba5c1360bafc3943f9d46be16b53774e1e859f34
Arturo Bravo <arturo@Arturos-MacBook-Air.local>
2021-05-26 13:09:59 -0700

    created doughnut for favorite games, still undecided on the title

--- src/graphs/RatioPlayed.js
+++ src/graphs/RatioPlayed.js
@@ -6,15 +6,47 @@ const RatioPlayed = ({games}) => {
         totalTime += games[i].playtime_forever;
     }
     console.log(totalTime)
+    let avgTime = totalTime/games.length;
+    avgTime = avgTime/1.5; //determines what qualifies to be graphed
     let gamesSort = games;
     //https://stackoverflow.com/questions/979256/sorting-an-array-of-objects-by-property-values
     gamesSort.sort((a, b) => (b.playtime_forever) - (a.playtime_forever));
+    let gameNames = [];
+    let playTime = [];
+    for(let i=0; i<gamesSort.length; i++){
+        if(gamesSort[i].playtime_forever >= avgTime){
+            gameNames.push(gamesSort[i].name);
+            playTime.push(gamesSort[i].playtime_forever);
+        }
+    }
+    const options = {
+        responsive: true,
+        plugins: {
+        title: {
+            display: true,
+            text: 'Favorite Games(?)',
+            color: 'white',
+            position: 'top'
+        },
+        legend: {
+            display: false
+        }
+    }
+    };
+    const data = {
+        labels: gameNames,
+        datasets: [{
+          label: 'Playtime',
+          data: playTime,
+          backgroundColor: '#6c757d',
+        }]
+    };
 
 
 
     return(
         <div>
-
+            <Doughnut data={data} options={options} />
         </div>
     )
 }
